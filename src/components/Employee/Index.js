480964348c6490ed5dd04261d581f16b7b074fd2
AndressaFL <andressadefariaslazoti@gmail.com>
2022-11-22 16:52:01 -0600

    Fix bug cabecalho

--- src/components/Employee/Index.js
+++ src/components/Employee/Index.js
@@ -1,19 +1,17 @@
-import "./Employee.css"
+import "./Employee.css";
 
 //using props , same as (props)
-const Employee = ({name, image, position}) => {
-    return(
-        <div className='employee'>
-            <div className='cabecalho'>
-                <img src={image} alt=''/>
-            </div>
-        <div className='footer'>
-            <h4>{name}</h4>
-            <h5>{position}</h5>
-        </div>
-        </div>
-
-    )
-
-}
-export default Employee
+const Employee = ({ name, image, position }) => {
+  return (
+    <div className="employee">
+      <div className="cabecalho">
+        <img src={image} alt="" />
+      </div>
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{position}</h5>
+      </div>
+    </div>
+  );
+};
+export default Employee;
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,18 +1,27 @@
-import Employee from '../Employee/Index'
-import './Team.css'
+import Employee from "../Employee/Index";
+import "./Team.css";
 
-const Team = (props)=>{
-
-    return(
-        //if true it will return the section
-        props.employees.length > 0 && 
-        <section className='team' style={{backgroundColor: props.colorSecondary}}>
-            <h3 style={{borderColor: props.colorPrimary}}>{props.name}</h3>
-        <div className='employees'>
-            {props.employees.map(employee => <Employee name={employee.name} position={employee.position} image={employee.image}/>)}
+const Team = (props) => {
+  return (
+    //if true it will return the section
+    props.employees.length > 0 && (
+      <section
+        className="team"
+        style={{ backgroundColor: props.colorSecondary }}>
+        <h3 style={{ borderColor: props.colorPrimary }}>{props.name}</h3>
+        <div className="employees">
+          <div className="cabecalho"
+            style={{ backgroundColor: props.colorPrimary }}>
+            {props.employees.map((employee) => (
+              <Employee
+                name={employee.name}
+                position={employee.position}
+                image={employee.image}/>
+            ))}
+          </div>
         </div>
-        </section>
+      </section>
     )
-
-}
-export default Team
+  );
+};
+export default Team;
